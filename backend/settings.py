f7dcead6b4eac58980ef650184b2ea8eeaee067f
EricRobertCampbell <eric.robert.campbell@gmail.com>
2021-02-09 17:21:19 -0700

    CORS issues
    - Fixed CORS issues for the frontend

--- backend/settings.py
+++ backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "backend.api",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -125,3 +127,7 @@ STATIC_URL = "/static/"
 GRAPHENE = {
     "SCHEMA": "backend.schema.schema",
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
